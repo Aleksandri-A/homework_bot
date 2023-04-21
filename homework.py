7fff43555370572a5075e4fec46a60a53e216968
Александра Никитенко <aleksandri@cn.ru>
2023-04-21 16:16:01 +0700

    Review2

--- homework.py
+++ homework.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import logging
-from http import HTTPStatus
 import requests
 import time
 import telegram
 
 from dotenv import load_dotenv
+from http import HTTPStatus
+from sorcery import dict_of
 
 
 load_dotenv()
@@ -43,17 +44,17 @@ HOMEWORK_VERDICTS = {
 }
 
 
+class HttpException(Exception):
+    pass
+
+
 def check_tokens():
     """Проверка доступности переменных окружения."""
-    if PRACTICUM_TOKEN is None:
-        logger.critical('Переменная PRACTICUM_TOKEN не задана.')
-        raise ValueError('Учетные данные не были предоставлены.')
-    if TELEGRAM_TOKEN is None:
-        logger.critical('Переменная TELEGRAM_TOKEN не задана.')
-        raise ValueError('Учетные данные не были предоставлены.')
-    if TELEGRAM_CHAT_ID is None:
-        logger.critical('Переменная TELEGRAM_CHAT_ID не задана.')
-        raise ValueError('Учетные данные не были предоставлены.')
+    dict = dict_of(PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
+    for i in dict:
+        if dict[i] is None:
+            logger.critical(f'Переменная {i} не задана.')
+            raise ValueError('Учетные данные не были предоставлены.')
 
 
 def send_message(bot, message):
@@ -72,20 +73,14 @@ def send_message(bot, message):
 def get_api_answer(timestamp):
     """Делает запрос к единственному эндпоинту API-сервиса."""
     PARAMS = {'from_date': timestamp}
-    try:
-        response = requests.get(
-            ENDPOINT,
-            headers=HEADERS,
-            params=PARAMS
-        )
-    except Exception as error:
-        logger.error(
-            f'Ошибка при отправке запроса к эндпоинту API-сервиса: {error}'
-        )
+    response = requests.get(
+        ENDPOINT,
+        headers=HEADERS,
+        params=PARAMS
+    )
     if response.status_code != HTTPStatus.OK:
         status_code = response.status_code
-        logger.error(f'Ответ с {ENDPOINT} не получен')
-        raise ConnectionError(f'Неверный код статуса: {status_code}')
+        raise HttpException(f'Неверный код статуса: {status_code}')
     logger.info('Отправка запроса успешно завершена. '
                 'Получен корректный ответ.')
     return response.json()
@@ -94,8 +89,9 @@ def get_api_answer(timestamp):
 def check_response(response):
     """Проверяет ответ API на соответствие документации."""
     if not isinstance(response, dict):
-        logger.info('Некорректный ответ от API!')
-        raise TypeError('Ответ не является словарем')
+        raise TypeError(
+            'Некорректный ответ от API! Ответ не является словарем'
+        )
     homeworks = response.get('homeworks')
     current_date = response.get('current_date')
     if homeworks is None:
@@ -103,28 +99,31 @@ def check_response(response):
     if current_date is None:
         raise ValueError('Ответ не содержит значение "current_date"')
     if not isinstance(homeworks, list):
-        logger.info('Значение "homeworks" не является списком!')
-        raise TypeError('Ответ типа данных!')
+        raise TypeError(
+            'Некорректный ответ от API! '
+            'Значение "homeworks" не является списком!'
+        )
     if not isinstance(current_date, int):
-        logger.info('Значение "current_date" не является целым числом!')
-        raise TypeError('Ошибка типа данных!')
-    return True
+        raise TypeError(
+            'Некорректный ответ от API! '
+            'Значение "current_date" не является целым числом!'
+        )
 
 
 def parse_status(homework):
     """Извлекает статус конкретной домашней работы."""
-    try:
-        homework_name = homework.get('homework_name')
-        status = homework.get('status')
-        if homework_name is None or status is None:
-            logger.error('Ошибка при запросе данных - отсутствуют ключи.')
-            raise TypeError(
-                f'Ошибка получения данных homework_name:'
-                f'{homework_name} и status: {status}'
-            )
-        verdict = HOMEWORK_VERDICTS[status]
-    except Exception as error:
-        logger.error(f'Неожиданный статус домашней работы: {error}')
+    if not isinstance(homework, dict):
+        raise TypeError(
+            'Некорректный ответ от API! Ответ не является словарем'
+        )
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if homework_name is None or status is None:
+        raise TypeError(
+            f'Ошибка получения данных homework_name:'
+            f'{homework_name} и status: {status}'
+        )
+    verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -139,9 +138,7 @@ def main():
         try:
             response = get_api_answer(timestamp)
             timestamp = response.get('current_date')
-            if check_response(response) is not True:
-                logger.error('Получен невалидный ответ')
-                continue
+            check_response(response)
             homeworks = response.get('homeworks')
             if len(homeworks) == 0:
                 logger.debug('Отсутствие в ответе новых статусов')
